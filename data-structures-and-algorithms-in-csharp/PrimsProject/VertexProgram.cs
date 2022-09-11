92382dacac531b5c54688bf2d49623159b972575
Renal Ahmedov <renalahmedov4@gmail.com>
2022-09-12 00:06:26 +0300

    uploading trees and graphs

---
+++ data-structures-and-algorithms-in-csharp/PrimsProject/VertexProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrimsProject
+{
+    class Vertex
+    {
+        public String name;
+        public int status;
+        public int predecessor;
+        public int length;
+
+        public Vertex(String name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ data-structures-and-algorithms-in-csharp/WarshallsProject/VertexProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WarshallsProject
+{
+    class Vertex
+    {
+        public String name;
+
+        public Vertex(String name)
+        {
+            this.name = name;
+        }
+    }
+}
